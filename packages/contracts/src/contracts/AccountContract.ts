781e09a49ae111753555c65198decfb9f9acf942
zkzoomer <sergio@zkzoomer.io>
2024-08-01 18:55:11 +0800

    rename to AccountContract

--- packages/contracts/src/contracts/AccountContract.ts
+++ packages/contracts/src/contracts/AccountContract.ts
@@ -1,7 +1,7 @@
 import { SmartContract } from "o1js"
 import type { EcdsaProgramProof } from "../programs/ecdsa"
 
-export class Account extends SmartContract {
+export class AccountContract extends SmartContract {
   _verifySignatureProof(signatureProof: EcdsaProgramProof) {
     signatureProof.verify()
     return signatureProof.publicOutput
