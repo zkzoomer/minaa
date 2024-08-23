d636611accd7f7e55e85a500e904e4b2e728d28c
zkzoomer <sergio@zkzoomer.io>
2024-08-23 18:49:37 +0200

    add EntryPoint

---
+++ packages/contracts/src/contracts/EntryPoint.ts
@@ -0,0 +1,201 @@
+import {
+    AccountUpdate,
+    Experimental,
+    Field,
+    Poseidon,
+    PublicKey,
+    State,
+    Struct,
+    UInt64,
+    Void,
+    method,
+    state,
+} from "o1js"
+import { DepositedEvent, IEntryPoint, UserOperationEvent, WithdrawnEvent } from "../interfaces/IEntryPoint"
+import { NonceSequence, UserOperation } from "../interfaces/UserOperation"
+import { AccountContract } from "./AccountContract"
+
+// Offchain storage definition
+const { OffchainState, OffchainStateCommitments } = Experimental
+export const entryPointOffchainState = OffchainState({
+    nonceSequenceNumber: OffchainState.Map(NonceSequence, Field),
+    depositInfo: OffchainState.Map(PublicKey, UInt64),
+})
+export class EntryPointStateProof extends entryPointOffchainState.Proof {}
+
+export class EntryPoint extends IEntryPoint {
+    events = {
+        Deposited: DepositedEvent,
+        Withdrawn: WithdrawnEvent,
+        UserOperation: UserOperationEvent,
+    };
+
+    // Offchain storage commitment
+    @state(OffchainStateCommitments) offchainState = State(
+        OffchainStateCommitments.empty()
+    )
+
+    /// @inheritdoc IEntryPoint
+    async getNonce(sender: PublicKey, key: Field): Promise<Field> {
+        return (await entryPointOffchainState.fields.nonceSequenceNumber.get({ sender, key })).orElse(Field(0))
+    }
+
+    /// @inheritdoc IEntryPoint
+    async balanceOf(account: PublicKey): Promise<UInt64> {
+        return (await entryPointOffchainState.fields.depositInfo.get(account)).orElse(UInt64.from(0))
+    }
+
+    /// @inheritdoc IEntryPoint
+    @method
+    async depositTo(account: PublicKey, amount: UInt64): Promise<Void> {
+        // Deposit the amount to the smart contract
+        AccountUpdate.createSigned(this.sender.getAndRequireSignature()).send({ to: this, amount });
+
+        // Update the offchain state
+        const oldAmount = await this.balanceOf(account)
+        await entryPointOffchainState.fields.depositInfo.update(account, {
+            from: oldAmount,
+            to: oldAmount.add(amount)
+        })
+
+        // Emits a `Deposited` event
+        this.emitEvent('Deposited', new DepositedEvent({ account, amount }))
+    }
+
+    /// @inheritdoc IEntryPoint
+    @method
+    async withdrawTo(
+        account: PublicKey,
+        recipient: PublicKey,
+        amount: UInt64,
+    ): Promise<Void> {
+        // Update the offchain state
+        const oldAmount = await this.balanceOf(account)
+        oldAmount.assertGreaterThanOrEqual(amount)
+        await entryPointOffchainState.fields.depositInfo.update(account, {
+            from: oldAmount,
+            to: oldAmount.sub(amount)
+        })
+
+        // Withdraw the amount to the recipient
+        AccountUpdate.createSigned(this.address).send({ to: recipient, amount });
+
+        // Emits a `Withdrawn` event
+        this.emitEvent('Withdrawn', new WithdrawnEvent({ account, recipient, amount }))
+    }
+
+    /// @inheritdoc IEntryPoint
+    @method
+    async handleOp(userOp: UserOperation, beneficiary: PublicKey): Promise<Void> {
+        const requiredPrefund = await this._getRequiredPrefund(userOp)
+        const userOpHash = await this._validatePrepayment(userOp, requiredPrefund)
+        await this._executeUserOp(userOp)
+        await this._compensate(beneficiary, requiredPrefund)
+
+        // Emits a `UserOperation`
+        this.emitEvent('UserOperation', new UserOperationEvent({ userOpHash, ...userOp }))
+    }
+
+    /// @inheritdoc IEntryPoint
+    @method.returns(Field)
+    async getUserOpHash(userOp: UserOperation): Promise<Field> {
+        const hash = Poseidon.hashPacked(UserOperation, userOp)
+        return Poseidon.hashPacked(Struct({ hash: Field, address: PublicKey }), { hash, address: this.address })
+    }
+
+    /**
+     * Validates a nonce uniqueness for the given account, called just after validateUserOp(). Reverts if the nonce is not valid
+     * @param sender account being validated
+     * @param key nonce key being validated
+     * @param _nonce nonce being validated
+     */
+    private async _validateAndUpdateNonce(
+        sender: PublicKey,
+        key: Field,
+        _nonce: Field,
+    ) {
+        // Get current nonce
+        const nonce = await this.getNonce(sender, key)
+        nonce.assertEquals(_nonce)
+
+        // Update offchain state
+        entryPointOffchainState.fields.nonceSequenceNumber.update({ sender, key }, {
+            from: nonce,
+            to: nonce.add(Field(1))
+        })
+    }
+
+    /**
+     * Validates account and ensures there is enough funds in the contract to pay for the gas fee
+     */
+    private async _validatePrepayment(userOp: UserOperation, requiredPrefund: UInt64): Promise<Field> {
+        await this._validateAndUpdateNonce(userOp.sender, userOp.key, userOp.nonce)
+        return this._validateAccountPrepayment(userOp, requiredPrefund)
+    }
+
+    /**
+     * Calls `validateUserOp` on the corresponding account, reverts if failed validation or no required prefund
+     * @param userOp 
+     * @param requiredPrefund 
+     */
+    private async _validateAccountPrepayment(userOp: UserOperation, requiredPrefund: UInt64): Promise<Field> {
+        // Compute the `userOpHash`
+        const userOpHash = await this.getUserOpHash(userOp)
+
+        // Get the amount that must be prefunded to complete this operation, if any
+        const balance = await this.balanceOf(userOp.sender)
+        const missingAccountFunds = balance.greaterThan(requiredPrefund) ? UInt64.from(0) : requiredPrefund.sub(balance)
+
+        // Validate the operation and receive the missing funds, if any
+        const accountContract = new AccountContract(userOp.sender)
+        await accountContract.validateUserOp(userOp, userOpHash, missingAccountFunds)
+
+        // Decrease the deposited amount for the account by the required prefund, which will be sent to the beneficiary
+        const oldAmount = await this.balanceOf(userOp.sender)
+        await entryPointOffchainState.fields.depositInfo.update(userOp.sender, {
+            from: oldAmount,
+            to: oldAmount.sub(requiredPrefund)
+        })
+
+        return userOpHash
+    }
+
+    /**
+     * Executes a user operation
+     * @param userOp the user operation being executed
+     */
+    private async _executeUserOp(userOp: UserOperation){
+        const accountContract = new AccountContract(userOp.sender)
+        await accountContract.execute(userOp.calldata.recipient, userOp.calldata.amount)
+    }
+
+    /**
+     * Returns the specified transaction fee in the user operation
+     * @param userOp the user operation being executed
+     * @returns transaction fee
+     */
+    private async _getRequiredPrefund(userOp: UserOperation): Promise<UInt64> {
+        return userOp.fee
+    }
+
+    /**
+     * Compensates the caller beneficiary address with the collected fee
+     * @param beneficiary address to receive the fees
+     * @param amount amount to receive
+     */
+    private async _compensate(beneficiary: PublicKey, amount: UInt64) {
+        AccountUpdate.createSigned(this.address).send({
+            to: beneficiary,
+            amount,
+        })
+    }
+
+    /**
+     * Method used to resolve pending state updates
+     * @param proof Recursive proof being verified
+     */
+    @method
+    async settle(proof: EntryPointStateProof) {
+        await entryPointOffchainState.settle(proof) 
+    }
+}
---
+++ packages/contracts/src/interfaces/IEntryPoint.ts
@@ -0,0 +1,92 @@
+import {
+    Field,
+    PublicKey,
+    SmartContract,
+    Struct,
+    UInt64,
+    Void,
+} from "o1js"
+import { UserOperation } from "./UserOperation"
+
+/**
+ * An event emitted after a given `amount` is credited to an account
+ * @param account the account being credited
+ * @param amount the amount being credited
+ */
+export class DepositedEvent extends Struct({
+    account: PublicKey,
+    amount: UInt64,
+}) {}
+
+/**
+ * An event emitted after a given `amount` is withdrawn from an account
+ * @param account the account being withdrawn from
+ * @param recipient the recipient of the withdrawal
+ * @param amount the amount being withdrawn
+ */
+export class WithdrawnEvent extends Struct({
+    account: PublicKey,
+    recipient: PublicKey,
+    amount: UInt64,
+}) {}
+
+/***
+ * An event emitted after each successful request
+ * @param userOpHash unique identifier for the request (hash its entire content, except signature)
+ * @param sender the account that generates this request
+ * @param key the nonce key value from the request
+ * @param nonce the nonce value from the request
+ */
+export class UserOperationEvent extends Struct({
+    userOpHash: Field,
+    sender: PublicKey,
+    key: Field,
+    nonce: Field, 
+}) {}
+
+export abstract class IEntryPoint extends SmartContract {
+    /**
+     * Returns the next valid nonce number for a given nonce key
+     * @param sender sender of the transaction
+     * @param key nonce key
+     * @returns the next valid nonce number
+     */
+    abstract getNonce(sender: PublicKey, key: Field): Promise<Field>
+
+    /**
+     * Gets the deposited amount for an account
+     * @param account account being checked
+     * @returns the deposited amount
+     */
+    abstract balanceOf(account: PublicKey): Promise<UInt64>
+
+    /**
+     * Adds to the deposit of the given account
+     * @param account account being topped up
+     * @param amount amount being topped up
+     */
+    abstract depositTo(account: PublicKey, amount: UInt64): Promise<Void>
+
+    /**
+     * Withdraws from an account's deposit
+     * @param account account being withdrawn from
+     * @param recipient account receiving the withdrawn amount
+     * @param amount amount being withdrawn 
+     */
+    abstract withdrawTo(account: PublicKey, recipient: PublicKey, amount: UInt64): Promise<Void>
+
+    /**
+     * Executes a `UserOperation`
+     * @param userOp user operation being executed
+     * @param beneficiary address to receive the fees
+     */
+    abstract handleOp(userOp: UserOperation, beneficiary: PublicKey): Promise<Void>
+
+    /**
+     * Generate a request ID - unique identifier for this request
+     * the request ID is a hash over the content of the userOp (except the signature), the entrypoint and the chainid
+     * @param userOp user operation being executeds
+     * @returns the request ID
+     */
+    abstract getUserOpHash(userOp: UserOperation): Promise<Field>
+}
