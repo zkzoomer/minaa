a2daf9863cb1be50f2475aade646030efe8c9dbc
zkzoomer <sergio@zkzoomer.io>
2024-08-24 12:56:20 +0200

    modify AccountFactory

--- packages/contracts/src/contracts/AccountFactory.ts
+++ packages/contracts/src/contracts/AccountFactory.ts
@@ -1,15 +1,29 @@
 import {
     AccountUpdate,
     Experimental,
+    Field,
     Permissions,
     PublicKey,
     SmartContract,
     State,
+    Struct,
+    UInt64,
     method,
     state,
 } from "o1js"
 import { Secp256k1 } from "../interfaces/UserOperation"
 import { AccountContract } from "./AccountContract"
+
+/**
+ * An event emitted after a given account `sender` was deployed
+ * @param sender the account that is deployed
+ * @param factory the factory used to deploy this account
+ */
+export class AccountDeployedEvent extends Struct({
+    sender: PublicKey,
+    factory: PublicKey,
+}) {}
+
 
 // Offchain storage definition
 const { OffchainState, OffchainStateCommitments } = Experimental
@@ -20,18 +34,38 @@ export class AccountFactoryStateProof extends accountFactoryOffchainState.Proof 
 const accountVerificationKey = (await AccountContract.compile()).verificationKey
 
 export class AccountFactory extends SmartContract {
+    events = {
+        AccountDeployed: AccountDeployedEvent,
+    }
+
+    // `EntryPoint` contract
+    @state(PublicKey)
+    entryPoint = State<PublicKey>()
     // Offchain storage commitment
     @state(OffchainStateCommitments) offchainState = State(
         OffchainStateCommitments.empty()
     )
 
     /**
+     * Initializes the `AccountFactory` smart contract
+     * @param entryPoint The `EntryPoint` smart contract
+     */
+    @method
+    async initialize(entryPoint: PublicKey) {
+        // Check that the `AccountFactory` was not already initialized
+        this.entryPoint.getAndRequireEquals().assertEquals(PublicKey.empty())
+        // Define the account's `entryPoint`
+        this.entryPoint.set(entryPoint)
+    }
+
+    /**
      * Creates an account at the specified address
      * @param owner The secp256k1 public key of the owner of the account, which must not be already defined
      * @param address The address where the account will be deployed to
+     * @param prefund amount the account is prefunded with
      */
     @method
-    public async createAccount(owner: Secp256k1, address: PublicKey) {
+    public async createAccount(owner: Secp256k1, address: PublicKey, prefund: UInt64) {
         // Check if account already exists, in which case transaction is reverted it
         const publicKey = await this.getPublicKey(owner)
         publicKey.assertEquals(PublicKey.empty())
@@ -45,11 +79,21 @@ export class AccountFactory extends SmartContract {
         })
         zkapp.account.verificationKey.set(accountVerificationKey)
 
+        // Initialize the account smart contract
+        const accountContract = new AccountContract(address)
+        await accountContract.initialize(this.entryPoint.getAndRequireEquals(), owner)
+
         // Update offchain state
         accountFactoryOffchainState.fields.accounts.update(owner, {
             from: publicKey,
             to: address
         })
+
+        // Prefund the account with the transaction amount
+        AccountUpdate.createSigned(this.sender.getAndRequireSignature()).send({ to: this, amount: prefund });
+
+        // Emit an `AccountDeployed` event
+        this.emitEvent('AccountDeployed', new AccountDeployedEvent({ sender: this.sender.getAndRequireSignature(), factory: this.address }))
     }
 
     /**
@@ -58,9 +102,7 @@ export class AccountFactory extends SmartContract {
      * @returns The public key of the account
      */
     async getPublicKey(owner: Secp256k1): Promise<PublicKey> {
-        const address = await accountFactoryOffchainState.fields.accounts.get(owner)
-
-        return address.orElse(PublicKey.empty())
+        return (await accountFactoryOffchainState.fields.accounts.get(owner)).orElse(PublicKey.empty())
     }
 
     /**
